99a36d5e30240806461a0cb1e0dc7193a378a74d
my3vshenoy <shenoymy31@gmail.com>
2017-12-27 13:53:10 -0500

    Added slider logic, outlets and IBAction

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Created an outlet for slider object
+    @IBOutlet weak var sliderObj: UISlider!
+    var currentValue = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       currentValue = lroundf(sliderObj.value)
+        print("Slider Value: \(currentValue)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     //My first code base
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello World", message: "Welcome to the Bulls Eye Game", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome", style:.default, handler: nil)
+        let alert = UIAlertController(title: "Hello World", message: "The value is \(currentValue)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style:.default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
 
 }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+        print("Slider Value: \(currentValue)")
+        
+    }
 }
