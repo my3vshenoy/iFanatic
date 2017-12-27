a10eac0105259a4c21425cca6f9610b214d4df35
my3vshenoy <shenoymy31@gmail.com>
2017-12-27 12:11:01 -0500

    Added a comment

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    //My first code base
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello World", message: "Welcome to the Bulls Eye Game", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style:.default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
 
 }
-
+}
