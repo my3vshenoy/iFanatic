65c27418491f075c1028c9114b3661bfd2aee923
my3vshenoy <shenoymy31@gmail.com>
2018-01-08 14:51:55 -0500

    Dev-Profile-My3 Project created

---
+++ App3-Swift4 Dev-Profile-My3/Dev-Profile-My3/Dev-Profile-My3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dev-Profile-My3
+//
+//  Created by My3 Shenoy on 08/01/18.
+//  Copyright © 2018 com.aarven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
