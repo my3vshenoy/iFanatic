552a2a4ca8e0af846b81932a5275199c35ec2e23
my3vshenoy <shenoymy31@gmail.com>
2018-01-08 14:30:46 -0500

    Adding Model file and logic for next button on LeagueVC

---
+++ App2-Swift4-Swoosh/Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by My3 Shenoy on 08/01/18.
+//  Copyright © 2018 com.aarven. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var selectedSkillLevel: String?
+    var desiredLeague: String?
+}
+
--- App2-Swift4-Swoosh/Swoosh/Swoosh/View Controller/LeagueVC.swift
+++ App2-Swift4-Swoosh/Swoosh/Swoosh/View Controller/LeagueVC.swift
@@ -10,19 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var nextBtn: BorderButton!
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        player = Player()
     }
 
-   
+    //MARK: IBActions
     @IBAction func onNextButtonTapped(_ sender: Any) {
         
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
     
-
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        self.selectedLeague(leagueType: "mens")
+    }
+    
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        self.selectedLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        self.selectedLeague(leagueType: "coed")
+    }
+    
+    
+    //MARK: Functions
+    func selectedLeague(leagueType: String){
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
    
 
 }
