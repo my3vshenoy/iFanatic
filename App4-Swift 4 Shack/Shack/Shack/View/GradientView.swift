a124cd74f0c1118c1d79a475ca531fde70421ce3
my3vshenoy <shenoymy31@gmail.com>
2018-01-08 21:20:08 -0500

    Channel View UI ready with Gradient layer

---
+++ App4-Swift 4 Shack/Shack/Shack/View/GradientView.swift
@@ -0,0 +1,38 @@
+//
+//  GradientView.swift
+//  Shack
+//
+//  Created by My3 Shenoy on 08/01/18.
+//  Copyright © 2018 com.aarven. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8588235294, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1){
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+
+    
+    override func layoutSubviews() {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x:1, y:1)
+        gradientLayer.frame = self.bounds
+        
+        //Insert the gradient into the view's layer
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
